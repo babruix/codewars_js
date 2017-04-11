15525b25f909dbd17112a42162a55032a6968f73
babruix <romanalexey@gmail.com>
2017-04-11 09:44:44 +0300

    My solution for "Is a number prime?" kata.

---
+++ isPrime.js
@@ -0,0 +1,26 @@
+/**
+ * Description:
+ *
+ * Is Prime
+ *
+ * Define a function isPrime that takes one integer argument and returns true or false depending on if the integer is a prime.
+ *
+ * Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+ *
+ * Example
+ *
+ * isPrime(5)
+ * => true
+ * Assumptions
+ *
+ * You can assume you will be given an integer input.
+ * You can not assume that the integer will be only positive. You may be given negative numbers.
+ */
+function isPrime(num) {
+    let i = num, count = 0
+    while (i > 0) {
+        if (!(num % i)) count++
+        i--
+    }
+    return count === 2
+}

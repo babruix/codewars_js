70b62300bde8cf62c16bc9fbdfe77d05b810fe33
babruix <romanalexey@gmail.com>
2017-04-19 01:52:56 +0300

    My solution for "RGB To Hex Conversion" kata.

---
+++ rgbToHex.js
@@ -0,0 +1,21 @@
+/**
+ * Description:
+ *
+ * The rgb() method is incomplete.
+ * Complete the method so that passing in RGB decimal values
+ * will result in a hexadecimal representation being returned.
+ * The valid decimal values for RGB are 0 - 255.
+ * Any (r,g,b) argument values that fall out of that range
+ * should be rounded to the closest valid value.
+ *
+ * The following are examples of expected output values:
+ *
+ * rgb(255, 255, 255) // returns FFFFFF
+ * rgb(255, 255, 300) // returns FFFFFF
+ * rgb(0,0,0) // returns 000000
+ * rgb(148, 0, 211) // returns 9400D3
+ *
+ */
+let hex = c => (c < 0 ? 0 : c > 255 ? 255 : c).toString(16).toUpperCase()
+let cmpToHex = c => hex(c).length === 1 ? "0" + hex(c) : hex(c)
+let rgb = (r, g, b) => cmpToHex(r) + cmpToHex(g) + cmpToHex(b)

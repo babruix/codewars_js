295a4926f678de7b2dccde5202e4f57cb4e195d5
babruix <romanalexey@gmail.com>
2017-04-20 11:21:18 +0300

    My solution for "Valid Parentheses" kata.

---
+++ validParentheses.js
@@ -0,0 +1,22 @@
+/**
+ * Description:
+ *
+ * Write a function called validParentheses that takes a string of parentheses,
+ * and determines if the order of the parentheses is valid.
+ * validParentheses should return true if the string is valid,
+ * and false if it's invalid.
+ *
+ * Examples:
+ * validParentheses( "()" ) => returns true
+ * validParentheses( ")(()))" ) => returns false
+ * validParentheses( "(" ) => returns false
+ * validParentheses( "(())((()())())" ) => returns true
+ *
+ * All input strings will be nonempty, and will only consist of open parentheses '('
+ * and/or closed parentheses ')'
+ */
+const validParentheses = parentheses => !removePatternRecursive(parentheses).length
+const removePatternRecursive = (string, pattern = '()') =>
+    string.split(pattern).length > 1
+        ? removePatternRecursive(string.split(pattern).join(''))
+        : string
